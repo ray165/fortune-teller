e85a5f2149eb709326ee6a8e6421ef1a0434be32
PinghuaLiu <crystalliu9438@gmail.com>
2023-11-17 00:27:00 -0800

    Updated schema

--- models/user.js
+++ models/user.js
@@ -1,16 +1,20 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
     username: {
         type: String,
         required: true,
     },
     password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
         type: String,
         required: true,
     },
