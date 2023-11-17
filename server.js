8e8ea07ce190364e38c85ed94c5a3689d7e9a741
Raymond <70726900+ray165@users.noreply.github.com>
2023-11-16 22:13:32 -0800

    add skeleton for project structure for node js express

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Sample users for testing
+const users = [
+    { id: 1, username: 'john', password: '123' },
+    { id: 2, username: 'admin', password: '111' }
+];
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/src/landing.html');
+});
+
+
+// Routes for user login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if the user exists and the password is correct
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        // Set user session and send success response with redirect URL
+        const redirectURL = username === 'admin' ? '/admin_dashboard' : '/user_dashboard';
+        res.json({ success: true, redirect: redirectURL });
+    } else {
+        // Send failure response
+        res.json({ success: false });
+    }
+});
+
+// Implement routes for admin and user dashboards similarly
+
+// Admin dashboard
+app.get('/admin_dashboard', (req, res) => {
+    res.send('Welcome to Admin Dashboard');
+});
+
+// User dashboard
+app.get('/user_dashboard', (req, res) => {
+    res.send('Welcome to User Dashboard');
+});
+
+app.get('*',function (req, res) {
+    res.redirect('/');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
