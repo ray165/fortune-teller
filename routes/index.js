70c0e2d4246f1d09f47a06304437761f481501f0
April Cheng <acheng118@my.bcit.ca>
2023-11-17 13:02:58 -0800

    Fix landing page path in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', function(req, res){
     console.log("GET request received for the root route.");
-    console.log("request", req);
-    console.log("response", res);
-    res.sendFile(__dirname + '/src/landing.html');
+    // console.log("request", req);
+    // console.log("response", res);
+
+    let landingPagePath = path.join(__dirname, '../', 'src', 'landing.html');
+    res.sendFile(landingPagePath);
 });
 
 module.exports = router;
