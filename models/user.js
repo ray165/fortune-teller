eefdb8047201b523d9f81d8e9a9ff433956f4e74
April Cheng <acheng118@my.bcit.ca>
2023-11-17 22:05:10 -0800

    Include stats in User model

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    stats: {
+        type: Object,
+        default: {},
+    }
 })
 
 module.exports = model('User', userSchema);
