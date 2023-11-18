e4fe3edd76831f8ec589a5df64949e5bf21fc5f3
April Cheng <acheng118@my.bcit.ca>
2023-11-17 22:44:07 -0800

    Add api template

---
+++ routes/api.js
@@ -0,0 +1,54 @@
+const { default: axios } = require('axios');
+const { verifyAccessToken } = require('../utils/tokens')
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+
+router.get('/', async (req, res) => {
+    res.send('Hello 👋, this is API endpoint');
+});
+
+router.post('/question', async (req, res) => {
+
+    try {
+        const MODEL_URL = '';
+        const { token, question } = req.body;
+        const { validToken, user_id } = await verifyAccessToken(req, res, token);
+
+        if (!validToken){
+            return res.status(404).json({ message: "Unauthorized" });
+        }
+
+        // call model endpoint
+        const postData = {
+            "api-key": process.env.API_KEY,
+            "quesion": question
+        }
+
+        const response = await axios.post(MODEL_URL, postData);
+
+        // update endpoint count
+        await User.findOneAndUpdate(
+            { _id: user_id},
+            { $inc: { 'stats.api/question': 1}},
+            { new: true}
+        );
+
+        res.status(200).json({
+			message: response.data,
+			type: 'success',
+		})
+
+    } catch (err) {
+        console.log("Error: calling model endpoint: ", err);
+
+        res.status(500).json({
+			type: 'error',
+			message: 'Error creating user!',
+			error,
+		})
+    }
+});
+
+module.exports = router;
