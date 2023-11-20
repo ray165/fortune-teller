ca6f33405fb8e3aeb67bb6db454c82d8191825b5
April Cheng <acheng118@my.bcit.ca>
2023-11-19 16:28:47 -0800

    Update api.js for token

--- routes/api.js
+++ routes/api.js
@@ -10,46 +10,69 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/question', async (req, res) => {
-    isUserAuthenticated(req, res);
+    const cookies = req.headers.cookie;
 
-    try {
-        const MODEL_URL = '';
-        const { token, question } = req.body;
-        const { validToken, user_id } = await verifyAccessToken(req, res, token);
+    if (cookies) {
+        // Parse cookies
+        const parsedCookies = cookies.split(';').reduce((acc, cookie) => {
+          const [key, value] = cookie.trim().split('=');
+          acc[key] = value;
+          return acc;
+        }, {});
+    
+        const token = parsedCookies.token; // get the cookie we want
+        console.log("TOKEN RECEIVED: ", token)
 
-        if (!validToken){
-            return res.status(404).json({ message: "Unauthorized" });
+        try {
+            const MODEL_URL = '';
+            const question = req.body.message;
+            console.log("question: ", question);
+            const { validToken, user_id } = await verifyAccessToken(token);
+    
+            if (!validToken){
+                return res.status(404).json({ message: "Unauthorized" });
+            }
+    
+            // call model endpoint
+            const postData = {
+                "api-key": process.env.API_KEY,
+                "quesion": question
+            }
+    
+            // const response = await axios.post(MODEL_URL, postData);
+    
+            // update endpoint count
+            await User.findOneAndUpdate(
+                { _id: user_id},
+                { 
+                    $inc: { 'stats.api/question.count': 1},
+                    $set: { 'stats.api/question.method': 'POST' }
+                },
+                { new: true}
+            );
+    
+            res.status(200).json({
+                // message: response.data,
+                message: 'Dummy data from server model call',
+                type: 'success',
+            })
+    
+        } catch (err) {
+            console.log("Error: calling model endpoint: ", err);
+    
+            res.status(500).json({
+                type: 'error',
+                message: 'Error creating user!',
+                error,
+            })
         }
 
-        // call model endpoint
-        const postData = {
-            "api-key": process.env.API_KEY,
-            "quesion": question
-        }
+    } else {
+        console.log("NO COOKIES DETECTED")
 
-        const response = await axios.post(MODEL_URL, postData);
-
-        // update endpoint count
-        await User.findOneAndUpdate(
-            { _id: user_id},
-            { 
-                $inc: { 'stats.api/question.count': 1},
-                $set: { 'stats.api/question.method': 'POST' }
-            },
-            { new: true}
-        );
-
-        res.status(200).json({
-			message: response.data,
-			type: 'success',
-		})
-
-    } catch (err) {
-        console.log("Error: calling model endpoint: ", err);
-
-        res.status(500).json({
+        res.status(400).json({
 			type: 'error',
-			message: 'Error creating user!',
+			message: 'missing cookie',
 			error,
 		})
     }
