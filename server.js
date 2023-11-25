6b9675d8ce16a2c8762260e582b9dbf5070aab60
Raymond <70726900+ray165@users.noreply.github.com>
2023-11-24 19:23:36 -0800

    update cors origin to our fe

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-    origin: "*",
+    origin: "https://fortune-teller-fe.onrender.com/, https://fortune-teller-fe.onrender.com/*",
     credentials: true
 }
 
